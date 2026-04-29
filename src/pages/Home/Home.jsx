959ef2d0ce81730749a5aef9fe951e47c993a12b
Daniel <lopezsantajustinadaniel@gmail.com>
2026-04-29 21:46:07 +0200

    Diseño Home finalizado, a falta de añadir las funcionalidades

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { logout, getLocalSession } from "../../services/authService";
+import { getLocalSession } from "../../services/authService";
+import { FaInstagram, FaFacebook, FaEnvelope } from "react-icons/fa";
 import "./Home.css";
 import logoFalla from "../../assets/logofalla.png";
 
@@ -21,6 +22,8 @@ function Home() {
 
   return (
     <div className="app-layout">
+
+      {/* HEADER */}
       <header className="home-header-modern">
         <div className="header-left-content">
           <div className="logo-container-small">
@@ -48,6 +51,7 @@ function Home() {
         </div>
       </header>
 
+      {/* CONTENT */}
       <main className="main-content">
         <div className="glass-card-compact">
           <h3>Tauler d'Anuncis</h3>
@@ -56,10 +60,19 @@ function Home() {
 
         <div className="spacer-40"></div>
 
-        <button className="btn-logout-minimal" onClick={() => { logout(); navigate("/"); }}>
-          Tancar Sessió
-        </button>
       </main>
+
+      {/* FOOTER */}
+      <div className="footer-container">
+        <p className="footer-copy">
+          © {new Date().getFullYear()} Falla Amics de Nàquera
+        </p>
+        <div className="redes-sociales">
+          <a href="https://www.instagram.com/falla_amics_naquera/" target="_blank" rel="noreferrer"><FaInstagram /></a>
+          <a href="https://www.facebook.com/p/Falla-Amics-de-N%C3%A1quera-100064316606908/?locale=es_ESbook.com" target="_blank" rel="noreferrer"><FaFacebook /></a>
+          <a href="mailto:falla_amics_naquera@hotmail.com?subject=Contacto Falla Amics"><FaEnvelope /></a>
+        </div>
+      </div>
     </div>
   );
 }
